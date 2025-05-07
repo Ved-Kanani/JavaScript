8b1db8ed01da1d5053e1d42cd0a7ad89878787d0
Ved-Kanani <vedkanani03@gmail.com>
2025-05-07 10:20:55 +0530

    If-else Statement.

--- If-else.js
+++ If-else.js
@@ -1,7 +1,18 @@
-let age = 18;
-if (age >= 18) {
-    console.log("You can vote.");
+// let age = 18;
+// if (age >= 18) {
+//     console.log("You can vote.");
+// }
+// if (age < 18) {
+//     console.log("You cannot vote.");
+// }
+
+let age= 61;
+if(age >=18 && age <=60){
+    console.log("You can drive");
 }
-if (age < 18) {
-    console.log("You cannot vote.");
+else if(age >60){
+    console.log("Your age is too much.");
+}
+else if(age <18){
+    console.log("your age is under 18.you can't drive.");
 }
