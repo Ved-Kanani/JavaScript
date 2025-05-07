ac1d74228d708ea5fafc841e5675118090980fdc
Ved-Kanani <vedkanani03@gmail.com>
2025-05-07 10:27:05 +0530

    Perforn a Practice Question.

---
+++ Practice-Q5.js
@@ -0,0 +1,13 @@
+let size = "XL";
+if(size == "S"){
+    console.log("Price is 50");
+}
+else if(size == "M"){
+    console.log("price is 100 ");
+}
+else if(size == "L"){
+    console.log("price is 200 ");
+}
+else if(size == "XL"){
+    console.log("price is 250 ");
+}
