3a43587dddac25d258cd60c79d8d0a4a75f0235d
Ved-Kanani <vedkanani03@gmail.com>
2025-05-08 10:31:47 +0530

    using swich-case statement.

---
+++ Switch.js
@@ -0,0 +1,15 @@
+let color="green";
+switch(color){
+    case "red":
+        console.log("stop");
+        break;
+    case "green":
+        console.log("go");
+        break;
+    case "yellow":
+        console.log("wait");
+        break;
+    default:
+        console.log("invalid color");
+        break;
+}
